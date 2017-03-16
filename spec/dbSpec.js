8fd11cb188cc613ad8907c044a24c8c919d12b35
MarcManiez <marc.maniez@gmail.com>
2017-03-15 20:51:48 -0700

    Create database spec.

---
+++ spec/dbSpec.js
@@ -0,0 +1,13 @@
+const expect = require('chai').expect;
+
+const db = require('../connection');
+
+describe('Database Controller', () => {
+  it('should correct detect if a link is inside the database', () => {
+    // expect(detectService('https://itun.es/us/8FRu')).to.equal('apple');
+  });
+
+  it('should correct detect if a link is not inside the database', () => {
+    // expect(detectService('https://itun.es/us/8FRu')).to.equal('apple');
+  });
+});
