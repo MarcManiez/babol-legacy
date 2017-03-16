9d088c623797fe5379087d8f2fec06dd5263d1fa
MarcManiez <marc.maniez@gmail.com>
2017-03-16 09:08:05 -0700

    Tests what properties a results should contain.

--- spec/dbSpec.js
+++ spec/dbSpec.js
@@ -9,26 +9,32 @@ describe('Database Controller', () => {
   describe('searchLink', () => {
     const error = new Error('Please provide a link and a service');
 
-    it('should return an error if one or fewer arguments are provided', () => {
-      searchLink().then().catch((err) => { expect(err).to.eql(error); });
+    it('should return an error if one or fewer arguments are provided', (done) => {
+      searchLink().then().catch((err) => { expect(err).to.eql(error); done(); });
     });
 
-    it('should correctly detect if a link is inside the database', () => {
+    it('should correctly detect if a link is inside the database', (done) => {
       searchLink('https://itun.es/us/nZ-wz?i=425454830', 'apple')
-      .then((link) => { expect(link).to.equal(true); })
-      .catch((err) => { expect(err).to.equal(true); });
+      .then((link) => {
+        expect(link.spotify).to.equal('https://open.spotify.com/track/2OWKDnQje6CyuUHtOWVuD9');
+        expect(link.apple).to.equal('https://itun.es/us/nZ-wz?i=425454830');
+        expect(link.artist.name).to.equal('Aaron Goldberg');
+        expect(link.album.name).to.equal('Turning Point');
+        expect(link.song.name).to.equal('Fantasy in D');
+        done();
+      });
     });
 
-    it('should correctly detect if a link is being requested from the wrong service', () => {
+    it('should correctly detect if a link is being requested from the wrong service', (done) => {
       searchLink('https://open.spotify.com/track/2OWKDnQje6CyuUHtOWVuD9', 'apple')
       .then()
-      .catch((err) => { expect(err).to.equal(null); });
+      .catch((err) => { expect(err).to.be.falsy; done(); });
     });
 
-    it('should correctly detect if a link is not inside the database', () => {
+    it('should correctly detect if a link is not inside the database', (done) => {
       searchLink('https://itun.es/us/kD24B', 'apple')
       .then()
-      .catch((err) => { expect(err).to.equal(null); });
+      .catch((err) => { expect(err).to.be.falsy; done(); });
     });
   });
 });
