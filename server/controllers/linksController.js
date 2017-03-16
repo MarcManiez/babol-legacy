2a28edf898de5271db831fd5fa69805112e75a8c
MarcManiez <marc.maniez@gmail.com>
2017-03-16 00:42:45 -0700

    Create method to seach database for a link.

--- server/controllers/linksController.js
+++ server/controllers/linksController.js
@@ -1,16 +1,33 @@
-const serviceRegexes = {
-  apple: /^https?:\/\/itun\.es\/\S+/g,
-  spotify: /^https?:\/\/play\.spotify\.com\/\S+/g,
-};
+const Link = require('../../database/models/link');
 
 module.exports = {
   detectService(link) {
+    const error = new Error('Invalid link or unsupported service.');
+    if (!link) return error;
+    const serviceRegexes = {
+      apple: /^https?:\/\/itun\.es\/\S+/g,
+      spotify: /^https?:\/\/play\.spotify\.com\/\S+/g,
+    };
     for (const service in serviceRegexes) {
       if (link.match(serviceRegexes[service])) return service;
     }
-    return new Error('Invalid link or unsupported service.');
+    return error;
   },
-  searchLink(link) {
-
+  searchLink(link, service) {
+    return new Promise((resolve, reject) => {
+      if (arguments.length <= 1) reject(new Error('Invalid link or unsupported service.'));
+      Link.where({ [service]: link }).fetch({ withRelated: ['artist', 'song', 'album'] })
+      .then((linkInstance) => {
+        // console.log(JSON.parse(JSON.stringify(linkInstance)));
+        console.log('LINK INSTANCE', linkInstance);
+        if (linkInstance === null) reject(linkInstance);
+        else {
+          resolve(linkInstance);
+        }
+      })
+      .catch((err) => {
+        reject(err);
+      });
+    });
   },
 };
