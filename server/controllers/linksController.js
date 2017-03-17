9d283ca16387ddd75d5d32abf80ca05569194e42
MarcManiez <marc.maniez@gmail.com>
2017-03-16 18:55:24 -0700

    Promisfy detectService and redo tests.

--- server/controllers/linksController.js
+++ server/controllers/linksController.js
@@ -3,25 +3,29 @@ const helpers = require('./helpers');
 
 module.exports = {
   detectService(link) {
-    const error = new Error('Invalid link or unsupported service.');
-    if (!link) return error;
-    const serviceRegexes = {
-      apple: /^https?:\/\/itun\.es\/\S+/g,
-      spotify: /^https?:\/\/play\.spotify\.com\/\S+/g,
-    };
-    for (const service in serviceRegexes) {
-      if (link.match(serviceRegexes[service])) return service;
-    }
-    return error;
+    return new Promise((resolve, reject) => {
+      const error = new Error('Invalid link or unsupported service.');
+      if (!link) reject(error);
+      const serviceRegexes = {
+        apple: /^https?:\/\/itun\.es\/\S+/g,
+        spotify: /^https?:\/\/play\.spotify\.com\/\S+/g,
+      };
+      for (const service in serviceRegexes) {
+        if (link.match(serviceRegexes[service])) resolve(service);
+      }
+      return reject(error);
+    });
   },
-  searchLink(link, service) {
-    return Link.where({ [service]: link }).fetch({ withRelated: ['artist', 'song', 'album'], require: true })
+
+  searchLink(link, service) { // TODO: this needs to be remade into a manual promise to reject when there are too few arguments.
+    return Link.where({ [service]: link }).fetch({ withRelated: ['artist', 'song', 'album'], require: true }) // TODO: empty responses need to be thenable.
     .then(linkInstance => helpers.formatLink(linkInstance));
   },
+
   post(req, res) {
     const link = req.body.link;
     const service = module.exports.detectService(link);
-    if (service.constructor === Error) res.status(404).render('404');
+    if (service.constructor === Error) res.status(404).render('404'); // TODO: this needs to be a promise. You'll have to redo your tests.
     return module.exports.searchLink(link, service)
     .then((linkInstance) => {
       console.log('LINKINSTANCE', linkInstance);
--- spec/linksSpec.js
+++ spec/linksSpec.js
@@ -5,19 +5,23 @@ const detectService = require('../server/controllers/linksController').detectSer
 describe('Links', () => {
   const error = new Error('Invalid link or unsupported service.');
 
-  it('should return an error if no link is provided', () => {
-    expect(detectService()).to.eql(error);
+  it('should return an error if no link is provided', (done) => {
+    detectService().then()
+    .catch((err) => { expect(err).to.eql(error); done(); });
   });
 
-  it('should correctly detect valid apple links', () => {
-    expect(detectService('https://itun.es/us/8FRu')).to.equal('apple');
+  it('should correctly detect valid apple links', (done) => {
+    detectService('https://itun.es/us/8FRu')
+    .then((service) => { expect(service).to.equal('apple'); done(); });
   });
 
-  it('should correctly detect valid spotify links', () => {
-    expect(detectService('https://play.spotify.com/artist/0BTfBwYC5Mw5ezDg91JBma')).to.equal('spotify');
+  it('should correctly detect valid spotify links', (done) => {
+    detectService('https://play.spotify.com/artist/0BTfBwYC5Mw5ezDg91JBma')
+    .then((service) => { expect(service).to.equal('spotify'); done(); });
   });
 
-  it('should correctly detect invalid links', () => {
-    expect(detectService('https://iun.e/us/8FRu')).to.eql(error);
+  it('should correctly detect invalid links', (done) => {
+    detectService('https://iun.e/us/8FRu').then()
+    .catch((err) => { expect(err).to.eql(error); done(); });
   });
 });
