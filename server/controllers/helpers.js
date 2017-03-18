65eb46cb98f1349a949868c2357eca921200f9c9
MarcManiez <marc.maniez@gmail.com>
2017-03-17 22:07:19 -0700

    Add a findOrCreate helper.

--- server/controllers/helpers.js
+++ server/controllers/helpers.js
@@ -1,3 +1,14 @@
 module.exports = {
+  services: ['apple', 'spotify'],
   formatLink: link => JSON.parse(JSON.stringify(link)), // TODO: think of a more efficient way to do this, eh?
+  findOrCreate(model, criteria) { // TODO: write some tests for this
+    return model.where(criteria).fetch()
+    .then((result) => {
+      if (!result) return model.forge(criteria).save();
+      return result;
+    });
+  },
+  findMissingServices(linkInstance) {
+
+  },
 };
