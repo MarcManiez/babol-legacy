2daac312664682534ed62738be0e559fa623a965
MarcManiez <marc.maniez@gmail.com>
2017-03-18 15:43:41 -0700

    Refactor findOrCreate helper.

--- server/controllers/helpers.js
+++ server/controllers/helpers.js
@@ -1,10 +1,10 @@
 module.exports = {
   services: ['apple', 'spotify'],
   formatLink: link => JSON.parse(JSON.stringify(link)), // TODO: think of a more efficient way to do this, eh?
-  findOrCreate(model, criteria) { // TODO: write some tests for this
+  findOrCreate(model, criteria) {
     return model.where(criteria).fetch()
     .then((result) => {
-      if (!result) return model.forge(criteria).save();
+      if (!result) return model.forge(criteria).save(null, { method: 'insert' });
       return result;
     });
   },
