358a093f4dcaa019acbd44aafa57ab2f70426a54
MarcManiez <marc.maniez@gmail.com>
2017-03-18 15:40:06 -0700

    Remove primary keys from seed data.

--- seeds/links_seed.js
+++ seeds/links_seed.js
@@ -4,17 +4,16 @@ exports.seed = (knex, Promise) => knex('links').del()
   .then(() => knex('albums').del())
   .then(() => knex('artists').del())
   .then(() => knex('artists').insert([
-    { id: 1, name: 'Aaron Goldberg' },
+    { name: 'Aaron Goldberg' },
   ]))
   .then(() => knex('albums').insert([
-    { id: 1, name: 'Turning Point', artist_id: 1 },
+    { name: 'Turning Point', artist_id: 1 },
   ]))
   .then(() => knex('songs').insert([
-    { id: 1, name: 'Fantasy in D', artist_id: 1, album_id: 1 },
+    { name: 'Fantasy in D', artist_id: 1, album_id: 1 },
   ]))
   .then(() => knex('links').insert([
     {
-      id: 1,
       apple: 'https://itun.es/us/nZ-wz?i=425454830',
       spotify: 'https://open.spotify.com/track/2OWKDnQje6CyuUHtOWVuD9',
       type: 'song',
@@ -23,7 +22,6 @@ exports.seed = (knex, Promise) => knex('links').del()
       song_id: 1,
     },
     {
-      id: 2,
       apple: 'https://itun.es/us/nZ-wz',
       spotify: 'https://open.spotify.com/album/1NYLLZQ0DBSMA6hDjonTnR',
       type: 'album',
@@ -31,7 +29,6 @@ exports.seed = (knex, Promise) => knex('links').del()
       album_id: 1,
     },
     {
-      id: 3,
       apple: 'https://itun.es/us/8FRu',
       spotify: 'https://open.spotify.com/artist/0BTfBwYC5Mw5ezDg91JBma',
       type: 'artist',
