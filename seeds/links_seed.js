b2f06b8036baf75d153474da0551f55a47b09a1e
MarcManiez <marc.maniez@gmail.com>
2017-03-18 19:14:22 -0700

    Fix indentation.

--- seeds/links_seed.js
+++ seeds/links_seed.js
@@ -1,37 +1,32 @@
-
-// knex('links').del()
-  // .then(() => knex('songs').del())
-  // .then(() => knex('albums').del())
-  // .then(() => knex('artists').del())
 exports.seed = (knex, Promise) => knex('artists').insert([
-    { name: 'Aaron Goldberg' },
-  ])
-  .then(() => knex('albums').insert([
-    { name: 'Turning Point', artist_id: 1 },
-  ]))
-  .then(() => knex('songs').insert([
-    { name: 'Fantasy in D', artist_id: 1, album_id: 1 },
-  ]))
-  .then(() => knex('links').insert([
-    {
-      apple: 'https://itun.es/us/nZ-wz?i=425454830',
-      spotify: 'https://open.spotify.com/track/2OWKDnQje6CyuUHtOWVuD9',
-      type: 'song',
-      artist_id: 1,
-      album_id: 1,
-      song_id: 1,
-    },
-    {
-      apple: 'https://itun.es/us/nZ-wz',
-      spotify: 'https://open.spotify.com/album/1NYLLZQ0DBSMA6hDjonTnR',
-      type: 'album',
-      artist_id: 1,
-      album_id: 1,
-    },
-    {
-      apple: 'https://itun.es/us/8FRu',
-      spotify: 'https://open.spotify.com/artist/0BTfBwYC5Mw5ezDg91JBma',
-      type: 'artist',
-      artist_id: 1,
-    },
-  ]));
+  { name: 'Aaron Goldberg' },
+])
+.then(() => knex('albums').insert([
+  { name: 'Turning Point', artist_id: 1 },
+]))
+.then(() => knex('songs').insert([
+  { name: 'Fantasy in D', artist_id: 1, album_id: 1 },
+]))
+.then(() => knex('links').insert([
+  {
+    apple: 'https://itun.es/us/nZ-wz?i=425454830',
+    spotify: 'https://open.spotify.com/track/2OWKDnQje6CyuUHtOWVuD9',
+    type: 'song',
+    artist_id: 1,
+    album_id: 1,
+    song_id: 1,
+  },
+  {
+    apple: 'https://itun.es/us/nZ-wz',
+    spotify: 'https://open.spotify.com/album/1NYLLZQ0DBSMA6hDjonTnR',
+    type: 'album',
+    artist_id: 1,
+    album_id: 1,
+  },
+  {
+    apple: 'https://itun.es/us/8FRu',
+    spotify: 'https://open.spotify.com/artist/0BTfBwYC5Mw5ezDg91JBma',
+    type: 'artist',
+    artist_id: 1,
+  },
+]));
