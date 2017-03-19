06a52d939a263c26da1c8bc746c188bf7ec13d30
MarcManiez <marc.maniez@gmail.com>
2017-03-18 19:26:14 -0700

    Clear nast console.log.

--- spec/linksSpec.js
+++ spec/linksSpec.js
@@ -98,7 +98,6 @@ describe('Links Controler', () => {
       request(server).post('/api/link').send({ link: 'https://itun.es/us/kQRMj?i=161135249' })
       .then((response) => {
         const page = response.text;
-        console.log('RESPONSE IN MAH TEST', response);
         const outcome = page.indexOf('La danse des canards') >= 0 && page.indexOf('La danse des canards - Single') >= 0 && page.indexOf('JJ Lionel') >= 0;
         expect(outcome).to.be.true;
         return Link.where({ type: 'song', apple: 'https://itun.es/us/kQRMj?i=161135249' }).fetch()
