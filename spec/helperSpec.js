225470781114b7e51ad4f7ca1a6f2598b8a09884
MarcManiez <marc.maniez@gmail.com>
2017-03-22 18:29:16 -0700

    Write more tests for isMatch.

--- spec/helperSpec.js
+++ spec/helperSpec.js
@@ -27,6 +27,30 @@ describe('Helper methods', () => {
       expect(helpers.isMatch('Art Blakey & The Jazz Messengers', 'Art Blakey and The Jazz Messengers')).to.be.at.least(0.8);
     });
 
+    it('should return a confidence score of 50% for two strings that are more than 50% similar', () => {
+      expect(helpers.isMatch('0000011111', '1111111111')).to.equal(0.5);
+    });
+
+    it('should return a confidence score of 0% for two strings with 0 similar characters', () => {
+      expect(helpers.isMatch('12345', '67890')).to.equal(0);
+    });
+
+    it('should return a confidence score of 20% for two strings with 1 similar characters and a length of 5', () => {
+      expect(helpers.isMatch('12345', '17890')).to.equal(0.2);
+    });
+
+    it('should return a confidence score of xx% for two strings that are more than xx% similar', () => {
+      expect(helpers.isMatch('12345', '67890')).to.equal(0);
+    });
+
+    it('should return a confidence score of 80% for two strings that are more than 80% similar', () => {
+      expect(helpers.isMatch('1111', '11111')).to.equal(0.8);
+    });
+
+    it('should return a confidence score of 60% for two strings that are more than 60% similar', () => {
+      expect(helpers.isMatch('1101', '11111')).to.equal(0.6);
+    });
+
     it('should return a confidence score < 80% for two strings that are less than 80% similar', () => {
       expect(helpers.isMatch('Art Blakey & The Jazz Messengers', 'Mingus Big Band')).to.be.below(0.8);
     });
