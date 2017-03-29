993dc4fef22c08800e5bafa8b70c030ab9fd4ae3
MarcManiez <marc.maniez@gmail.com>
2017-03-28 22:04:01 -0700

    Rename frontend-js folder to client.

--- client/index.js
+++ client/index.js
@@ -5,7 +5,7 @@ const app = {
     const httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = () => {
       if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) {
-        console.log(httpRequest.responseText);
+        app.update(JSON.parse(httpRequest.responseText));
       } else if (httpRequest.status !== 200 && httpRequest.readyState !== 1) {
         console.error(httpRequest);
       }
@@ -14,4 +14,20 @@ const app = {
     httpRequest.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     httpRequest.send(`link=${link}`);
   },
+  update(response) {
+    app.links.babol = `http://localhost:8000/link/${response.id}`;
+    for (let i = 0; i < app.services.length; i += 1) {
+      app.links[app.services[i]] = response[app.services[i]];
+    }
+    for (const type in app.content) {
+      app.content[type] = response[type].name;
+    }
+  },
+  content: {
+    artist: null,
+    album: null,
+    song: null,
+  },
+  links: {},
+  services: ['apple', 'spotify'],
 };
