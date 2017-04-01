84af19f2e4d7cdb3a798a34c9963e1a8978237a9
MarcManiez <marc.maniez@gmail.com>
2017-04-01 11:09:51 -0700

    Silence animation-related listeners.

--- client/event-listeners.js
+++ client/event-listeners.js
@@ -1,2 +1,17 @@
+// const slide = function () {
+//   // debugger;
+//   const sliders = document.getElementById('permalink').children;
+//   for (const node of sliders) {
+//     node.className += ' slide';
+//   }
+// };
+
 const form = document.querySelector('form');
 form.addEventListener('submit', app.getLinks);
+// form.addEventListener('submit', slide);
+
+const copy = document.getElementById('permalink').children[1];
+copy.onclick = function (e) {
+  document.getElementById('permalink').children[0].select();
+  document.execCommand('copy');
+};
