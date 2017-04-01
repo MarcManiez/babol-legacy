edd0a72fd1acc7ec3ae87be3d173e975d32f577b
MarcManiez <marc.maniez@gmail.com>
2017-04-01 11:23:43 -0700

    Assign content properties according to type.

--- client/index.js
+++ client/index.js
@@ -16,12 +16,17 @@ const app = {
     httpRequest.send(`link=${link}`);
   },
   update(response) {
+    const type = response.type;
     app.links.babol = `http://localhost:8000/link/${response.id}`;
     for (let i = 0; i < app.services.length; i += 1) {
       app.links[app.services[i]] = response[app.services[i]];
     }
-    for (const type in app.content) {
-      app.content[type] = response[type].name;
+    app.content.artist = response.artist.name;
+    if (type === 'album' || type === 'song') {
+      app.content.album = response.album.name;
+    }
+    if (type === 'song') {
+      app.content.song = response.song.name;
     }
     document.getElementById('permalink').children[0].value = app.links.babol.slice(7);
   },
