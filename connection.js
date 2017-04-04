163c41ea998bc9076bc056a74a8100506dde9f0b
MarcManiez <marc.maniez@gmail.com>
2017-04-03 23:53:17 -0700

    Clean up pseudo code.

--- connection.js
+++ connection.js
@@ -3,5 +3,3 @@ const config = require('./knexfile')[process.env.NODE_ENV === 'production' ? 'pr
 const db = require('knex')(config);
 
 module.exports = require('bookshelf')(db);
-
-// db.migrate.latest(config);
