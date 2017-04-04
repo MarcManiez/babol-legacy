6fd72bdaab8ae4567b10886e772dab50389c0091
MarcManiez <marc.maniez@gmail.com>
2017-04-03 22:33:37 -0700

    Correct env variable name to be checked.

--- connection.js
+++ connection.js
@@ -1,4 +1,4 @@
-const config = require('./knexfile')[process.env.PRODUCTION ? 'production' : 'development'];
+const config = require('./knexfile')[process.env.NODE_ENV ? 'production' : 'development'];
 
 const db = require('knex')(config);
 
