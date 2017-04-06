dd13db623fa41f09bac6df84b2a6ec5dd408c1b3
MarcManiez <marc.maniez@gmail.com>
2017-04-05 20:02:39 -0700

    Move middleware to separate file.

---
+++ server/middleware.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const morgan = require('morgan');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const routes = require('./routes');
+const getLink = require('./controllers/linksController').get;
+
+module.exports = (app) => {
+  app.use(morgan('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+};
--- server/server.js
+++ server/server.js
@@ -1,26 +1,29 @@
 const express = require('express');
 const path = require('path');
-const morgan = require('morgan');
+// const morgan = require('morgan');
+// const bodyParser = require('body-parser');
 const swig = require('swig');
-const bodyParser = require('body-parser');
 
 const db = require('../connection');
+const middleware = require('./middleware');
+
 const routes = require('./routes');
 const getLink = require('./controllers/linksController').get;
 
 const app = express();
 module.exports = app;
 
+// templating configuration
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
-
-// TODO: separate middleware into new file
-app.use(morgan('dev'));
-app.use(bodyParser.json());
 if (process.env.ENV !== 'production') {
   swig.setDefaults({ cache: false });
 }
-app.use(bodyParser.urlencoded({ extended: true }));
+
+// middleware invocation
+middleware(app);
+
+// TODO: separate middleware into new file
 app.use('*/client', express.static(path.join(__dirname, '../client')));
 app.get('/', (req, res) => res.render('index', { url: req.url }));
 app.get('/link/:id', getLink);
