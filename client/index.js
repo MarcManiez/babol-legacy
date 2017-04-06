5b38cb580c08c6df36f44f78b430c238b7327530
MarcManiez <marc.maniez@gmail.com>
2017-04-06 15:51:31 -0700

    Refactor getLinks method with http request helper.

--- client/index.js
+++ client/index.js
@@ -1,29 +1,19 @@
 const app = {
-  init() {
-      // do nothing
-        // cookies are the right tool for the job in this instance, because they are sent with every request
-        // this makes it a better experience when it comes to redirecting users to their streaming service.
-        // if we did it with JWT, there would first be a get request to /link/:id
-        // after that response, the javascript is read, and the JWT sent back to the server for a porential redirection. This is bad.
-        // Using a cookie, the correct information will be included from the get-go and the redirect will be seamless.
+  getService() {
+    // get the current value
+    // cookies are the right tool for the job in this instance, because they are sent with every request
+    // this makes it a better experience when it comes to redirecting users to their streaming service.
+    // if we did it with JWT, there would first be a get request to /link/:id
+    // after that response, the javascript is read, and the JWT sent back to the server for a porential redirection. This is bad.
+    // Using a cookie, the correct information will be included from the get-go and the redirect will be seamless.
+  },
+  setService() {
+
   },
   getLinks(event) {
-    return new Promise((resolve, reject) => {
-      event.preventDefault();
-      const link = document.getElementById('link').value;
-      const httpRequest = new XMLHttpRequest();
-      httpRequest.onreadystatechange = () => {
-        if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) {
-          resolve(JSON.parse(httpRequest.responseText));
-        } else if (httpRequest.status !== 200 && httpRequest.readyState !== 1) {
-          console.error(httpRequest);
-          reject(httpRequest);
-        }
-      };
-      httpRequest.open('POST', `${window.location.href}api/link/`, true);
-      httpRequest.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-      httpRequest.send(`link=${link}`);
-    });
+    event.preventDefault();
+    const link = document.getElementById('link').value;
+    return httpRequest('POST', `${window.location.href}api/link/`, { body: `link=${link}` });
   },
   update(response) {
     const type = response.type;
