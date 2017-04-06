7f18f4a40876a41b8a7ea02815dc59b60e6de78c
MarcManiez <marc.maniez@gmail.com>
2017-04-06 16:10:12 -0700

    Test cookie module.

--- server/middleware.js
+++ server/middleware.js
@@ -1,11 +1,20 @@
 const path = require('path');
 const morgan = require('morgan');
+const cookie = require('cookie');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const getLink = require('./controllers/linksController').get;
 
 module.exports = (app) => {
+  // app.use((req, res, next) => {
+  //   const sweetCookie = req.get('Cookie');
+  //   if (sweetCookie) {
+  //     console.log('yaya?');
+  //     console.log(sweetCookie);
+  //   }
+  //   next();
+  // });
   app.use(morgan('dev'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
