d291341f4f7fd1d1533adbf4c744c9ef2aa5df7b
MarcManiez <marc.maniez@gmail.com>
2017-04-08 16:10:01 -0700

    Adapt seed file to schema refactor.

--- seeds/links_seed.js
+++ seeds/links_seed.js
@@ -1,32 +1,5 @@
-exports.seed = (knex, Promise) => knex('artists').insert([
-  { name: 'Aaron Goldberg' },
-])
-.then(() => knex('albums').insert([
-  { name: 'Turning Point', artist_id: 1 },
-]))
-.then(() => knex('songs').insert([
-  { name: 'Fantasy in D', artist_id: 1, album_id: 1 },
-]))
-.then(() => knex('links').insert([
-  {
-    apple: 'https://itun.es/us/nZ-wz?i=425454830',
-    spotify: 'https://open.spotify.com/track/2OWKDnQje6CyuUHtOWVuD9',
-    type: 'song',
-    artist_id: 1,
-    album_id: 1,
-    song_id: 1,
-  },
-  {
-    apple: 'https://itun.es/us/nZ-wz',
-    spotify: 'https://open.spotify.com/album/1NYLLZQ0DBSMA6hDjonTnR',
-    type: 'album',
-    artist_id: 1,
-    album_id: 1,
-  },
-  {
-    apple: 'https://itun.es/us/8FRu',
-    spotify: 'https://open.spotify.com/artist/0BTfBwYC5Mw5ezDg91JBma',
-    type: 'artist',
-    artist_id: 1,
-  },
-]));
+exports.seed = (knex, Promise) => knex('artists').insert({ name: 'Aaron Goldberg', slug: '12345678', spotify_id: '1', apple_id: '1' }, 'id')
+.then(artists => knex('albums').insert(
+  { name: 'Turning Point', artist_id: artists[0], slug: '12345678', spotify_id: '2', apple_id: '2' }, 'id', 'artist_id'))
+.then(albums => knex('songs').insert(
+  { name: 'Fantasy in D', artist_id: albums[1], album_id: albums[0], slug: '12345678', spotify_id: '3', apple_id: '3' }, 'id'));
