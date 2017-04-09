d4012519a970d418b824804ba6cd6177b6780381
MarcManiez <marc.maniez@gmail.com>
2017-04-08 20:28:56 -0700

    Seed database with correct records.

--- seeds/links_seed.js
+++ seeds/links_seed.js
@@ -1,5 +1,5 @@
 exports.seed = (knex, Promise) => knex('artists').insert({ name: 'Aaron Goldberg', slug: '12345678', spotify_id: '1', apple_id: '1' }, 'id')
 .then(artists => knex('albums').insert(
-  { name: 'Turning Point', artist_id: artists[0], slug: '12345678', spotify_id: '2', apple_id: '2' }, 'id', 'artist_id'))
+  { name: 'Turning Point', artist_id: artists[0], slug: '12345678', spotify_id: '2', apple_id: '2' }, ['id', 'artist_id']))
 .then(albums => knex('songs').insert(
-  { name: 'Fantasy in D', artist_id: albums[1], album_id: albums[0], slug: '12345678', spotify_id: '3', apple_id: '3' }, 'id'));
+  { name: 'Fantasy in D', artist_id: albums[0].artist_id, album_id: albums[0].id, slug: '12345678', spotify_id: '3', apple_id: '3' }, 'id'));
