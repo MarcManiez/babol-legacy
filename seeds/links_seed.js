c345f3f749ebe5a622b35f58d8474ae6cf8a01a8
MarcManiez <marc.maniez@gmail.com>
2017-04-09 01:40:38 -0700

    Declare accurate properties for our seed data.

--- seeds/links_seed.js
+++ seeds/links_seed.js
@@ -1,5 +1,13 @@
 exports.seed = (knex, Promise) => knex('artists').insert({ name: 'Aaron Goldberg', slug: '12345678', spotify_id: '1', apple_id: '1' }, 'id')
 .then(artists => knex('albums').insert(
   { name: 'Turning Point', artist_id: artists[0], slug: '12345678', spotify_id: '2', apple_id: '2' }, ['id', 'artist_id']))
-.then(albums => knex('songs').insert(
-  { name: 'Fantasy in D', artist_id: albums[0].artist_id, album_id: albums[0].id, slug: '12345678', spotify_id: '3', apple_id: '3' }, 'id'));
+.then(albums => knex('songs').insert({
+  name: 'Fantasy in D',
+  artist_id: albums[0].artist_id,
+  album_id: albums[0].id,
+  slug: '12345678',
+  spotify_id: '2OWKDnQje6CyuUHtOWVuD9',
+  apple_id: '425454830',
+  apple_url: 'https://itun.es/us/nZ-wz?i=425454830',
+  spotify_url: 'https://open.spotify.com/track/2OWKDnQje6CyuUHtOWVuD9',
+}));
