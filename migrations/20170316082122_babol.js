5908ae621d89a4f6eb4b10ae2857ae7da2595d59
MarcManiez <marc.maniez@gmail.com>
2017-04-08 20:53:41 -0700

    Add urls to schema.

--- migrations/20170316082122_babol.js
+++ migrations/20170316082122_babol.js
@@ -3,16 +3,20 @@ exports.up = (knex, Promise) => Promise.all([
     artists.increments('id').primary();
     artists.string('slug').unique().notNullable();
     artists.string('name').notNullable();
-    artists.string('apple_id').unique().unsigned();
+    artists.string('apple_id').unique();
+    artists.string('apple_url').unique();
     artists.string('spotify_id').unsigned();
+    artists.string('spotify_url').unique();
     artists.timestamps(true);
   }),
   knex.schema.createTable('albums', (albums) => {
     albums.increments('id').primary();
     albums.string('slug').unique().notNullable();
     albums.string('name').notNullable();
-    albums.string('apple_id').unique().unsigned();
-    albums.string('spotify_id').unique().unsigned();
+    albums.string('apple_id').unique();
+    albums.string('apple_url').unique();
+    albums.string('spotify_id').unique();
+    albums.string('spotify_url').unique();
     albums.integer('artist_id').unsigned();
     albums.foreign('artist_id').references('artists.id');
     albums.timestamps(true);
@@ -21,8 +25,10 @@ exports.up = (knex, Promise) => Promise.all([
     songs.increments('id').primary();
     songs.string('slug').unique().notNullable();
     songs.string('name').notNullable();
-    songs.string('apple_id').unique().unsigned();
-    songs.string('spotify_id').unique().unsigned();
+    songs.string('apple_id').unique();
+    songs.string('apple_url').unique();
+    songs.string('spotify_id').unique();
+    songs.string('spotify_url').unique();
     songs.integer('artist_id').unsigned();
     songs.foreign('artist_id').references('artists.id');
     songs.integer('album_id').unsigned();
