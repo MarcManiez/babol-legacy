e5c0616a3869c5c65aac35cc2d129f77998e2f28
MarcManiez <marc.maniez@gmail.com>
2017-04-14 22:50:34 -0700

    Cast wider net in Apple Music API searches.

--- spec/linksSpec.js
+++ spec/linksSpec.js
@@ -135,7 +135,7 @@ describe('Links Controler', () => {
       .to.eventually.have.deep.property('body.name', 'Getz/Gilberto');
     });
 
-    it.only('should fetch missing links given a brand new spotify track link', () => {
+    it('should fetch missing links given a brand new spotify track link', () => {
       const task = request(server).post('/api/link').send({ link: 'https://open.spotify.com/track/0IDoJJD5rea4Em9JZA8Wh2' });
       const url = 'https://itunes.apple.com/us/album/el-negro-del-blanco/id448538868?i=448538886&uo=4';
       return Promise.all([
