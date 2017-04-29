105991ca575059d60c5eba1ebcf6de26baddd4ef
MarcManiez <marc.maniez@gmail.com>
2017-04-28 18:38:43 -0700

    Add a switch to manage database connection configurations.

--- connection.js
+++ connection.js
@@ -1,4 +1,15 @@
-const config = require('./knexfile')[process.env.NODE_ENV === 'production' ? 'production' : 'development'];
+let config = require('./knexfile');
+
+switch (process.env.NODE_ENV) {
+case 'production':
+  config = config.production;
+  break;
+case 'test':
+  config = config.test;
+  break;
+default:
+  config = config.development;
+}
 
 const knex = require('knex')(config);
 const bookshelf = require('bookshelf')(knex);
